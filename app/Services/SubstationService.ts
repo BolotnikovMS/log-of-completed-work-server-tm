494dbb1037aada6cc3a322841399262326feef9a
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-09 21:50:25 +0500

    feat: :zap: Сервис с методами для работы с substation

---
+++ app/Services/SubstationService.ts
@@ -0,0 +1,30 @@
+import { ActiveEnum } from 'App/Enums/Active'
+import { IQueryParams } from 'App/Interfaces/QueryParams'
+import { OrderByEnum } from 'App/Enums/Sorted'
+import { RequestContract } from "@ioc:Adonis/Core/Request"
+import Substation from 'App/Models/Substation'
+
+interface IParamGetSubstationBy {
+  colName: string
+  id: number
+  request: RequestContract
+}
+
+export default class SubstationService {
+  public static async getSubstationByRelationId({colName, id, request}: IParamGetSubstationBy) {
+    if (!id) throw new Error('Не передан id!')
+    if (!colName) throw new Error('Не передано название столбца для выборки!')
+
+    const { sort, order, active, offset = 0, limit = 10 } = request.qs() as IQueryParams
+    const substations = await Substation
+      .query()
+      .where(colName, '=', id)
+      .offset(offset)
+      .limit(limit)
+      .if(sort && order, query => query.orderBy(sort, OrderByEnum[order]))
+      .if(active, query => query.where('active', '=', ActiveEnum[active]))
+      .preload('voltage_class')
+
+    return substations
+  }
+}
