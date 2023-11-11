36b51deaf36e3b97758d29bf98909d6649b45292
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-11 21:59:43 +0500

    feat: :sparkles: Файлы с политиками проверки прав пользователя

---
+++ app/Policies/SubstationPolicy.ts
@@ -0,0 +1,20 @@
+import BasePolicy from 'App/Policies/BasePolicy'
+import { RolesEnum } from 'App/Enums/Roles'
+import User from 'App/Models/User'
+import { checkUserRole } from 'App/Utils/utils'
+
+export default class SubstationPolicy extends BasePolicy {
+	// public async viewList(user: User) {}
+	public async view(user: User) {
+    return [RolesEnum.USER, RolesEnum.MODERATOR].includes(user.roleId)
+  }
+	public async create(user: User) {
+    return checkUserRole(user, 'MODERATOR')
+  }
+	public async update(user: User) {
+    return checkUserRole(user, 'MODERATOR')
+  }
+	public async delete(user: User) {
+    return checkUserRole(user, 'MODERATOR')
+  }
+}
