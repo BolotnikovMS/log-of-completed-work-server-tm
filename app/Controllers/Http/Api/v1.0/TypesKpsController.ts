b5e909b8a26b0b6d3a86f0dd97b043d9ef404627
BolotnikovMS <bolotnikovms@yandex.ru>
2023-11-26 19:45:15 +0500

    feat: Добавлены параметры запроса

--- app/Controllers/Http/Api/v1.0/TypesKpsController.ts
+++ app/Controllers/Http/Api/v1.0/TypesKpsController.ts
@@ -1,14 +1,21 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { IQueryParams } from 'App/Interfaces/QueryParams'
+import { OrderByEnum } from 'App/Enums/Sorted'
 import SubstationService from 'App/Services/SubstationService'
 import TypeKpValidator from 'App/Validators/TypeKpValidator'
 import TypesKp from 'App/Models/TypesKp'
 
 export default class TypesKpsController {
-  public async index({ response }: HttpContextContract) {
+  public async index({ request, response }: HttpContextContract) {
     try {
-      const typesKp = await TypesKp.query()
+      const { sort, order, page, limit } = request.qs() as IQueryParams
+      const typesKp = await TypesKp
+        .query()
+        .if(sort && order, query => query.orderBy(sort, OrderByEnum[order]))
+        .if(page && limit, query => query.paginate(page, limit))
+      const total = (await TypesKp.query().count('* as total'))[0].$extras.total
 
-      return response.status(200).json(typesKp)
+      return response.status(200).json({ meta: {total}, data: typesKp })
     } catch (error) {
       return response.status(500).json({ message: 'Произошла ошибка при выполнении запроса!' })
     }
@@ -29,7 +36,7 @@ export default class TypesKpsController {
   public async store({ request, response, auth }: HttpContextContract) {
     try {
       const validatedData = await request.validate(TypeKpValidator)
-      const typeKp = await TypesKp.create({userId: auth?.user?.id, ...validatedData})
+      const typeKp = await TypesKp.create({userId: auth?.user?.id || 1, ...validatedData})
 
       return response.status(201).json(typeKp)
     } catch (error) {
