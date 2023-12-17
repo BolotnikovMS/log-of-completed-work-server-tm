886f10d5095750854282f2e56cf759f0e76d91b9
BolotnikovMS <bolotnikovms@yandex.ru>
2023-12-17 18:52:21 +0500

    feat: :sparkles: Добавлена обработка query params

--- app/Controllers/Http/Api/v1.0/ChannelTypesController.ts
+++ app/Controllers/Http/Api/v1.0/ChannelTypesController.ts
@@ -1,13 +1,20 @@
 import ChannelType from 'App/Models/ChannelType'
 import ChannelTypeValidator from 'App/Validators/ChannelTypeValidator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { IQueryParams } from 'App/Interfaces/QueryParams'
+import { OrderByEnum } from 'App/Enums/Sorted'
 
 export default class ChannelTypesController {
-  public async index({ response }: HttpContextContract) {
+  public async index({ request, response }: HttpContextContract) {
     try {
-      const channelTypes = await ChannelType.query()
+      const { sort, order, page, limit } = request.qs() as IQueryParams
+      const channelTypes = await ChannelType
+        .query()
+        .if(sort && order, query => query.orderBy(sort, OrderByEnum[order]))
+        .if(page && limit, query => query.paginate(page, limit))
+      const total = (await ChannelType.query().count('* as total'))[0].$extras.total
 
-      return response.status(200).json(channelTypes)
+      return response.status(200).header('total-count', total).json({ meta: {total}, data: channelTypes})
     } catch (error) {
       return response.status(500).json({ message: 'Произошла ошибка при выполнении запроса!' })
     }
@@ -16,7 +23,7 @@ export default class ChannelTypesController {
   public async store({ request, response, auth }: HttpContextContract) {
     try {
       const validatedData = await request.validate(ChannelTypeValidator)
-      const channelType = await ChannelType.create({userId: auth?.user?.id, ...validatedData})
+      const channelType = await ChannelType.create({userId: auth?.user?.id || 1, ...validatedData})
 
       return response.status(201).json(channelType)
     } catch (error) {
