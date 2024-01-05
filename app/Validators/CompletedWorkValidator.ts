fec9f04e6ab7f368af836bd5bc68bdba93bdf418
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-05 20:58:39 +0500

    fix: Убрана проверка формата

--- app/Validators/CompletedWorkValidator.ts
+++ app/Validators/CompletedWorkValidator.ts
@@ -1,4 +1,4 @@
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
 
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
@@ -10,13 +10,13 @@ export default class CompletedWorkValidator {
     workProducerId: schema.number(),
     description: schema.string([rules.trim(), rules.minLength(5), rules.maxLength(1000), rules.escape()]),
     note: schema.string.optional([rules.trim(), rules.minLength(3), rules.maxLength(700), rules.escape()]),
-    dateCompletion: schema.date({ format: 'dd.MM.yyyy' })
+    dateCompletion: schema.date()
   })
 
   public messages: CustomMessages = {
     required: 'Поле {{ field }} является обязательным.',
     minLength: 'Минимальная длина поля {{ field }} - {{ options.minLength }} символа.',
-    maxLength: 'Максимальная длина поля {{ field }} - {{ options.maxLength }} символа.',
+    maxLength: 'Максимальная длина поля {{ field }} - {{ options.maxLength }} символов.',
     'date.format': 'Дата и время должны иметь формат: {{ options.format }}.',
   }
 }
