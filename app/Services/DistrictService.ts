a28fbf36d4005e0f827985b65123268e1b0769e1
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-14 14:28:18 +0500

    feat: Добавлен метод getDistrictSubstations

--- app/Services/DistrictService.ts
+++ app/Services/DistrictService.ts
@@ -1,9 +1,7 @@
-import { RequestContract } from '@ioc:Adonis/Core/Request'
-import { ActiveEnum } from 'App/Enums/Active'
-import { OrderByEnum } from 'App/Enums/Sorted'
-import { IQueryParams } from 'App/Interfaces/QueryParams'
 import District from 'App/Models/District'
-import Substation from 'App/Models/Substation'
+import { IQueryParams } from 'App/Interfaces/QueryParams'
+import { OrderByEnum } from 'App/Enums/Sorted'
+import { RequestContract } from '@ioc:Adonis/Core/Request'
 
 export default class DistrictService {
 	public static async getDistricts(req: RequestContract): Promise<{meta: {total: number}, data: District[]}> {
@@ -15,18 +13,5 @@ export default class DistrictService {
 		const total: number = (await District.query().count('* as total'))[0].$extras.total
 
 		return { meta: {total}, data: districts }
-	}
-	public static async getDistrictSubstations(district: District, req: RequestContract): Promise<{meta: {total: number}, data: Substation[]}> {
-		const { active, sort, order, page, limit } = req.qs() as IQueryParams
-		const substations = await Substation
-			.query()
-			.where('district_id', '=', district.id)
-			.if(active, query => query.where('active', '=', ActiveEnum[active]))
-			.if(sort && order, query => query.orderBy(sort, OrderByEnum[order]))
-			.if(page && limit, query => query.paginate(page, limit))
-			.preload('voltage_class')
-		const total: number = (await Substation.query().count('* as total'))[0].$extras.total
-
-		return { meta: {total}, data: substations }
 	}
 }
