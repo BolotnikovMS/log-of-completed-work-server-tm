e60ec22676dd757dfa402c37d7fa836a21a36ada
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-14 14:27:47 +0500

    feat: Использование метода getSubstations

--- app/Controllers/Http/Api/v1.0/SubstationsController.ts
+++ app/Controllers/Http/Api/v1.0/SubstationsController.ts
@@ -1,30 +1,17 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { ActiveEnum } from 'App/Enums/Active'
-import { OrderByEnum } from 'App/Enums/Sorted'
 import { IQueryParams } from 'App/Interfaces/QueryParams'
 import CompletedWork from 'App/Models/CompletedWork'
 import Substation from 'App/Models/Substation'
+import SubstationService from 'App/Services/SubstationService'
 import SubstationValidator from 'App/Validators/SubstationValidator'
 
 export default class SubstationsController {
   public async index({ request, response, bouncer }: HttpContextContract) {
     try {
       // if (await bouncer.with('SubstationPolicy').denies('view')) return response.status(403).json({ message: 'Недостаточно прав для выполнения операции!' })
-      const { sort, order, page, limit, active } = request.qs() as IQueryParams
-      const substations = await Substation
-        .query()
-        .if(sort && order, query => query.orderBy(sort, OrderByEnum[order]))
-        .if(active, query => query.where('active', ActiveEnum[active]))
-        .if(page && limit, query => query.paginate(page, limit))
-        .preload('voltage_class')
-      const total = (await Substation.query().count('* as total'))[0].$extras.total
-      // const serializeSubstation = substations.map(substation => substation.serialize({
-      //   fields: {
-      //     pick: ['id', 'active', 'name', 'rdu', 'slug']
-      //   }
-      // }))
+			const substations = await SubstationService.getSubstations(request)
 
-      return response.status(200).header('total-count', total).json({ meta: {total}, data: substations })
+      return response.status(200).header('total-count', substations.meta.total).json(substations)
     } catch (error) {
       return response.status(500).json({ message: 'Произошла ошибка при выполнении запроса!' })
     }
@@ -47,69 +34,13 @@ export default class SubstationsController {
 
   public async getInfoSubstation({ params, response, bouncer }: HttpContextContract) {
     try {
-      if (await bouncer.with('SubstationPolicy').denies('view')) return response.status(403).json({ message: 'Недостаточно прав для выполнения операции!' })
-
+      // if (await bouncer.with('SubstationPolicy').denies('view')) return response.status(403).json({ message: 'Недостаточно прав для выполнения операции!' })
+			// !! Попробовать сделать запрос в сервисе
       const substation = await Substation.find(params.id)
 
       if (substation) {
-        await substation.load('district')
-        await substation.load('voltage_class')
-        await substation.load('type_kp')
-        await substation.load('head_controller')
-        await substation.load('main_channel')
-        await substation.load('backup_channel')
-        await substation.load('additional_channel')
-        await substation.load('gsm')
+				const serializeSubstation = await SubstationService.getSubstation(substation.id)
 
-        const serializeSubstation = substation.serialize({
-          fields: {
-            omit: ['user_id', 'district_id', 'voltage_classes_id', 'type_kp_id', 'head_controller_id', 'main_channel_id', 'backup_channel_id', 'additional_channel_id', 'gsm_id']
-          },
-          relations: {
-            district: {
-              fields: {
-                pick: ['id', 'name', 'short_name']
-              }
-            },
-            voltage_class: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-            type_kp: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-            head_controller: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-            main_channel: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-            backup_channel: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-            additional_channel: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-            gsm: {
-              fields: {
-                pick: ['id', 'name']
-              }
-            },
-          }
-        })
-
-        // console.log(substation.serialize())
         return response.status(200).json(serializeSubstation)
       }
       return response.status(404).json({ message: 'Не найдено!' })
