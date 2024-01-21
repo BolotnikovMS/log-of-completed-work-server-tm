e87e46ba0c8b860c793a209bdba0a13b81a26555
BolotnikovMS <bolotnikovms@yandex.ru>
2024-01-21 19:56:56 +0500

    refactor: Использование значение query string

--- app/Services/CompletedWorkService.ts
+++ app/Services/CompletedWorkService.ts
@@ -5,19 +5,18 @@ import { IQueryParams } from 'App/Interfaces/QueryParams'
 import CompletedWork from 'App/Models/CompletedWork'
 
 export default class CompletedWorkService {
-	public static async getCompletedWorks(req: RequestContract, substationId?: number | undefined ): Promise<{meta: {total: number}, data: ModelObject[]}> {
-		console.log(substationId)
-		const { sort = 'createdAt', order = 'desc', page, limit } = req.qs() as IQueryParams
+	public static async getCompletedWorks(req: RequestContract): Promise<{meta: {total: number}, data: ModelObject[]}> {
+		const { sort = 'createdAt', order = 'desc', page, limit, substation } = req.qs() as IQueryParams
 		const works = await CompletedWork
 			.query()
-			.if(substationId, query => query.where('substationId', '=', substationId!))
+			.if(substation, query => query.where('substationId', '=', substation))
 			.if(sort && order, query => query.orderBy(sort, OrderByEnum[order]))
 			.if(page && limit, query => query.paginate(page, limit))
 			.preload('work_producer')
 			.preload('substation', query => query.preload('voltage_class'))
 		const total: number = (await CompletedWork
 			.query()
-			.if(substationId, query => query.where('substationId', '=', substationId!))
+			.if(substation, query => query.where('substationId', '=', substation))
 			.count('* as total'))[0].$extras.total
 		const serializeWorks = works.map(work => {
 			return work.serialize({
